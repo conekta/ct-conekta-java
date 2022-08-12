f4eb626471c390dc925c809497e672d8a7a73372
afalla-conekta <andres.falla@conekta.com>
2022-08-12 10:07:08 -0500

    Ajustes conflictos merge develop

--- src/main/java/conekta/io/client/impl/OrdersClient.java
+++ src/main/java/conekta/io/client/impl/OrdersClient.java
@@ -8,7 +8,6 @@ import conekta.io.error.ConektaErrorResponse;
 import conekta.io.model.request.OrderReq;
 import conekta.io.model.response.Order;
 
-import java.io.IOException;
 import java.net.http.HttpResponse;
 
 public class OrdersClient extends ConektaRequestor {
@@ -19,8 +18,18 @@ public class OrdersClient extends ConektaRequestor {
      * @param orderReq The order request.
      * @return The order.
      */
-    public ConektaResponse<Order> createOrder(OrderReq orderReq){
+    public ConektaResponse<Order> createOrder(OrderReq orderReq) {
         HttpResponse<String> orderResponse = doRequest(orderReq, Constants.ORDERS_PATH, Constants.POST);
+        return ConektaResponse.<Order>builder()
+                .response(orderResponse)
+                .statusCode(orderResponse.statusCode())
+                .data(ConektaObjectMapper.getInstance().stringJsonToObject(orderResponse.body(), Order.class))
+                .error(ConektaObjectMapper.getInstance().stringJsonToObject(orderResponse.body(), ConektaErrorResponse.class))
+                .build();
+    }
+
+    public ConektaResponse<Order> updateOrder(String orderId, OrderReq orderReq) {
+        HttpResponse<String> orderResponse = doRequest(orderReq, Constants.ORDERS_PATH + Constants.SLASH + orderId, Constants.PUT);
         return ConektaResponse.<Order>builder()
                 .response(orderResponse)
                 .statusCode(orderResponse.statusCode())
