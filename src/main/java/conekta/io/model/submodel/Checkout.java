a00226e1eae3272434de4b8cd69410a947db5b62
Andres Mauricio Falla Castañeda <91920806+afalla-conekta@users.noreply.github.com>
2022-08-18 16:38:46 -0500

    Ajuste checkout Orders (#11)

--- src/main/java/conekta/io/model/submodel/Checkout.java
+++ src/main/java/conekta/io/model/submodel/Checkout.java
@@ -9,13 +9,20 @@ import java.util.List;
 @Data
 public class Checkout extends ConektaObject {
     private List<Integer> allowedInstallmentOptions;
+    private List<Integer> monthlyInstallmentsOptions;
     private List<String> allowedPaymentMethods;
+    private List<Object> excludeCardNetworks;
     private BigDecimal emailsSent;
+    private String successUrl;
+    private String failureUrl;
     private BigDecimal expiredAt;
     private BigDecimal expiresAt;
+    private BigDecimal startsAt;
     private Boolean force3dsFlow;
     private Boolean livemode;
     private Boolean monthlyInstallmentsEnabled;
+    private Boolean isRedirectOnFailure;
+    private Boolean canNotExpire;
     private String name;
     private Boolean needsShippingContact;
     private BigDecimal paidPaymentsCount;
@@ -25,4 +32,5 @@ public class Checkout extends ConektaObject {
     private String status;
     private String type;
     private String url;
+    private Object metadata;
 }
