9f7b7e23447e005078177f60a85eba740d66a46a
Andres Mauricio Falla Castañeda <andres.falla@conekta.com>
2022-09-02 08:59:58 -0500

    Refactor del servcio de ordenes

--- src/main/java/conekta/io/model/submodel/ShippingLines.java
+++ src/main/java/conekta/io/model/submodel/ShippingLines.java
@@ -2,9 +2,12 @@ package conekta.io.model.submodel;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class ShippingLines extends ConektaData {
     private String carrier;
     private String method;
     private String trackingNumber;
+    private BigDecimal amount;
 }
