7e065e9658b2d279aee2f275791d257c385d5d0b
Franklin Carrero <franklin.carrero@conekta.com>
2022-12-15 10:31:31 -0500

    add url in Checkout response (#36)

--- src/main/java/conekta/io/model/impl/Checkout.java
+++ src/main/java/conekta/io/model/impl/Checkout.java
@@ -1,6 +1,11 @@
 package conekta.io.model.impl;
 
 import conekta.io.model.ConektaObject;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class Checkout extends ConektaObject {
+    private String url;
 }
