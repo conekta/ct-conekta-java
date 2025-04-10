f54fc93e81e740faa3420ae4ee70536c093599d8
Martín Agüera <maguera@conekta.com>
2025-04-10 18:59:06 -0300

    The SDK version has been updated to 7.0.0 in README, VERSION, pom.xml, config-java.json, ApiClient.java, and Configuration.java. (#75)

--- src/main/java/com/conekta/Configuration.java
+++ src/main/java/com/conekta/Configuration.java
@@ -15,7 +15,7 @@ package com.conekta;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.5.0")
 public class Configuration {
-    public static final String VERSION = "6.1.4";
+    public static final String VERSION = "7.0.0";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
