4ba39935da2a9c4087dc22abc68a061ed6c490de
Franklin <franklin.carrero@conekta.com>
2026-06-30 11:25:02 -0500

    Bump version to 8.0.0 in README, POM, and configuration files

--- README.md
+++ README.md
@@ -41,7 +41,7 @@ Add this dependency to your project's POM:
 <dependency>
   <groupId>io.conekta</groupId>
   <artifactId>ct-conekta-java</artifactId>
-  <version>7.2.0</version>
+  <version>8.0.0</version>
   <scope>compile</scope>
 </dependency>
 ```
@@ -57,7 +57,7 @@ Add this dependency to your project's build file:
   }
 
   dependencies {
-     implementation "io.conekta:ct-conekta-java:7.2.0"
+     implementation "io.conekta:ct-conekta-java:8.0.0"
   }
 ```
 
@@ -71,7 +71,7 @@ mvn clean package
 
 Then manually install the following JARs:
 
-- `target/ct-conekta-java-7.2.0.jar`
+- `target/ct-conekta-java-8.0.0.jar`
 - `target/lib/*.jar`
 
 ## Usage
--- src/main/java/com/conekta/ApiClient.java
+++ src/main/java/com/conekta/ApiClient.java
@@ -99,7 +99,7 @@ public class ApiClient extends JavaTimeFormatter {
 
   private JSONObject getConektaUserAgent()  {
     JSONObject userAgent = new JSONObject();
-    userAgent.put("bindings_version", "7.2.0");
+    userAgent.put("bindings_version", "8.0.0");
     userAgent.put("lang", "java");
     userAgent.put("lang_version", System.getProperty("java.version"));
     userAgent.put("publisher", "conekta");
@@ -151,7 +151,7 @@ public class ApiClient extends JavaTimeFormatter {
     this.dateFormat = new RFC3339DateFormat();
 
     // Set default User-Agent.
-    setUserAgent("Conekta/v2 JavaBindings/7.2.0");
+    setUserAgent("Conekta/v2 JavaBindings/8.0.0");
 
     // Setup authentications (key: authentication name, value: authentication).
     authentications = new HashMap<>();
